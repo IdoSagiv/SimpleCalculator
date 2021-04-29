b088efda2f1ef73cd8d709fff18d8af10367ed7a
Ido Sagiv <ido.sagiv@mail.huji.ac.il>
2021-04-29 12:23:51 +0300

    assign the views the correct onClickListeners, initialize the output view, and deal with save/restore states

--- app/src/main/java/android/exercise/mini/calculator/app/MainActivity.java
+++ app/src/main/java/android/exercise/mini/calculator/app/MainActivity.java
@@ -6,11 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @VisibleForTesting
     public SimpleCalculator calculator;
+
+    private TextView textViewCalculatorOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,25 +25,70 @@ public class MainActivity extends AppCompatActivity {
             calculator = new SimpleCalculatorImpl();
         }
 
-    /*
-    TODO:
-    - find all views
-    - initial update main text-view based on calculator's output
-    - set click listeners on all buttons to operate on the calculator and refresh main text-view
-     */
+        // find views
+        textViewCalculatorOutput = findViewById(R.id.textViewCalculatorOutput);
+        TextView[] digitButtons = {findViewById(R.id.button0), findViewById(R.id.button1),
+                findViewById(R.id.button2), findViewById(R.id.button3), findViewById(R.id.button4),
+                findViewById(R.id.button5), findViewById(R.id.button6), findViewById(R.id.button7),
+                findViewById(R.id.button8), findViewById(R.id.button9)};
+        TextView buttonEquals = findViewById(R.id.buttonEquals);
+        TextView buttonPlus = findViewById(R.id.buttonPlus);
+        TextView buttonMinus = findViewById(R.id.buttonMinus);
+        View buttonBackSpace = findViewById(R.id.buttonBackSpace);
+        TextView buttonClear = findViewById(R.id.buttonClear);
+
+        // initial output value
+        textViewCalculatorOutput.setText(calculator.output());
+
+
+        // assign on click listener to the views
+        for (int i = 0; i < digitButtons.length; i++) {
+            digitButtons[i].setOnClickListener(digitButtonListener(i));
+        }
+
+        buttonBackSpace.setOnClickListener(v -> {
+            calculator.deleteLast();
+            textViewCalculatorOutput.setText(calculator.output());
+        });
+
+        buttonEquals.setOnClickListener(v -> {
+            calculator.insertEquals();
+            textViewCalculatorOutput.setText(calculator.output());
+        });
+
+        buttonPlus.setOnClickListener(v -> {
+            calculator.insertPlus();
+            textViewCalculatorOutput.setText(calculator.output());
+        });
+
+        buttonMinus.setOnClickListener(v -> {
+            calculator.insertMinus();
+            textViewCalculatorOutput.setText(calculator.output());
+        });
+
+        buttonClear.setOnClickListener(v -> {
+            calculator.clear();
+            textViewCalculatorOutput.setText(calculator.output());
+        });
     }
 
     @Override
     protected void onSaveInstanceState(@NonNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putSerializable("calculatorData",calculator.saveState());
-        // todo: save calculator state into the bundle
+        outState.putSerializable("calculatorData", calculator.saveState());
     }
 
     @Override
     protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         calculator.loadState(savedInstanceState.getSerializable("calculatorData"));
-        // todo: restore calculator state from the bundle, refresh main text-view from calculator's output
+        textViewCalculatorOutput.setText(calculator.output());
+    }
+
+    private View.OnClickListener digitButtonListener(int digit) {
+        return v -> {
+            calculator.insertDigit(digit);
+            textViewCalculatorOutput.setText(calculator.output());
+        };
     }
 }
